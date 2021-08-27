f7ba58e451f98f376723d113d4b699e53a63e891
douglas ruan de araujo fernando <67982779+Douglasghs@users.noreply.github.com>
2021-08-27 20:52:58 -0300

    Definindo projeto

---
+++ CSV_Read/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_Read
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.SafeFileName;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+    }
+}
